eca10aafc22c66aa94529883d6e5d1bbbf5cd0d1
Yuan-Man <68322456+Yuan-ManX@users.noreply.github.com>
2025-05-27 12:11:58 +0800

    Update __init__.py

--- __init__.py
+++ __init__.py
@@ -1,15 +1,15 @@
-from .nodes import LoadAniSoraModel, Prompt, AniSora, SaveAniSora
+from .nodes import LoadAniSoraModel, AniSoraPrompt, AniSora, SaveAniSora
 
 NODE_CLASS_MAPPINGS = {
     "LoadAniSoraModel": LoadAniSoraModel,
-    "Prompt": Prompt,
+    "AniSoraPrompt": AniSoraPrompt,
     "AniSora": AniSora,
     "SaveAniSora": SaveAniSora,
 }
 
 NODE_DISPLAY_NAME_MAPPINGS = {
     "LoadAniSoraModel": "Load AniSora Model",
-    "Prompt": "Prompt",
+    "AniSoraPrompt": "AniSora Prompt",
     "AniSora": "AniSora",
     "SaveAniSora": "Save AniSora",
 } 
